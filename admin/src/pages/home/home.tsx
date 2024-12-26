bdfb18991ad6fef5bc9a3e9291cdfe3fa2aee368
s1rne <s.simaranov8@gmail.com>
2024-12-26 14:58:56 +0300

    подключил первые три карточки к апи

--- admin/src/pages/home/home.tsx
+++ admin/src/pages/home/home.tsx
@@ -13,18 +13,37 @@ import { cardsRoute, editModRoute, homeRoute, setsRoute } from '@/shared/routes'
 import { Header } from '@/widgets/header'
 import { LinkCard } from '@/widgets/link-card'
 import { StatisticCard } from '@/widgets/statistic-card'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 export function HomePage() {
-  const newUsers = '+1036' // new users for the last 24 hours
-  const allUsers = '1 203 636' // all users
-  const cardsOpened = '10 593 737' // amount of cards opened in the last 24 hours
+
+  const [statistics, setStatistics] = useState<{
+    total_users: number,
+    new_users_last_24h: number,
+    cards_opened_last_24h: number,
+  } | null>(null);
+
+  useEffect(() => {
+    fetch(`http://0.0.0.0:4550/admin/statistics/total`, {
+      method: 'GET',
+    }).then(response => response.json())
+      .then(data => {
+        setStatistics(data);
+      });
+  }, [])
+  
+
+  // const newUsers = '+1036' // new users for the last 24 hours
+  // const allUsers = '1 203 636' // all users
+  // const cardsOpened = '10 593 737' // amount of cards opened in the last 24 hours
   return (
     <>
       <Header />
       <main className="w-full grid grid-cols-3 px-[60px] py-[30px] gap-x-[100px] gap-y-[50px]">
-        <StatisticCard heading="Новые пользователи за 24 часа" data={newUsers} icon={<GrowthIcon />} />
-        <StatisticCard heading="Общее количество пользователей" data={allUsers} icon={<PeopleIcon />} />
-        <StatisticCard heading="Карт открыто за 24 часа" data={cardsOpened} icon={<BoxIcon />} />
+        <StatisticCard heading="Новые пользователи за 24 часа" data={statistics?.new_users_last_24h.toLocaleString('ru-RU') || ''} icon={<GrowthIcon />} />
+        <StatisticCard heading="Общее количество пользователей" data={statistics?.total_users.toLocaleString('ru-RU') || ''} icon={<PeopleIcon />} />
+        <StatisticCard heading="Карт открыто за 24 часа" data={statistics?.cards_opened_last_24h.toLocaleString('ru-RU') || ''} icon={<BoxIcon />} />
         <LinkCard
           to={cardsRoute}
           params={{ id: 'new' }}
